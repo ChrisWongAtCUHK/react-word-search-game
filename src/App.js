f0faa85d272e4c91e3666610925d1ed282c07439
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-06 20:21:45 +0800

    Word list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.scss'
 
 function App() {
+  const matrix = [
+    ['A', 'M', 'C', 'N', 'T', 'S', 'K', 'B', 'A', 'N'],
+    ['D', 'O', 'A', 'H', 'T', 'O', 'T', 'A', 'L', 'S'],
+    ['A', 'J', 'N', 'X', 'E', 'V', 'R', 'U', 'O', 'R'],
+    ['P', 'C', 'D', 'U', 'P', 'E', 'F', 'R', 'N', 'E'],
+    ['R', 'B', 'Y', 'R', 'S', 'R', 'S', 'V', 'A', 'K'],
+    ['E', 'Y', 'O', 'G', 'U', 'R', 'T', 'E', 'R', 'C'],
+    ['T', 'L', 'N', 'I', 'B', 'A', 'R', 'S', 'G', 'A'],
+    ['Z', 'K', 'T', 'G', 'I', 'I', 'J', 'X', 'W', 'R'],
+    ['E', 'I', 'L', 'R', 'S', 'S', 'P', 'I', 'H', 'C'],
+    ['L', 'S', 'E', 'I', 'K', 'O', 'O', 'C', 'U', 'F'],
+  ]
+
+  const words = [
+    'BARS',
+    'CANDY',
+    'CARROT',
+    'CHEESE',
+    'CHIPS',
+    'COOKIES',
+    'CRACKERS',
+    'FRUIT',
+    'GRANOLA',
+    'NUTS',
+    'PRETZEL',
+    'YOGURT',
+  ]
+
+  const [foundWords, setFoundWords] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className='main-content word-game'>
+        <h2>Find these {words.length} words</h2>
+        <div className='word-search-game'>
+          <div className='words-list'>
+            {words.map((word) => (
+              <div key={word} className='words-list__item'>
+                <span className='words-list__value'>{word}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
